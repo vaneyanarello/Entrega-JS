4518ea335aeb882b1afbaba10422ee94776109b5
Vanesa Yanarello <vane.yanarello@gmail.com>
2025-12-10 00:08:23 +0100

    Eliminar comentarios y codigo en desuso

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
 //! PRODUCTOS
 
-// const productos = [
-//     {
-//     id: 1,
-//     nombre: "Remera",
-//     precio: 15000,
-//     talle: ["S", "M", "L"],
-//     },
-
-//     {
-//     id: 2,    
-//     nombre: "Pantalón",
-//     precio: 25000,
-//     talle: ["S", "M", "L"]
-//     },
-
-//     {
-//     id:3,
-//     nombre: "Buzo",
-//     precio: 20000,
-//     talle: ["S", "M", "L"]
-//     },
-
-//     {
-//     id:4,
-//     nombre: "Campera",
-//     precio: 50000,
-//     talle: ["S", "M", "L"]
-//     }
-// ];
-
 let productos = []; 
 
 async function cargarProductos() {
@@ -43,12 +13,14 @@ async function cargarProductos() {
         Swal.fire({
             icon: "error",
             title: "Oops...",
-            text: "Something went wrong!",
-            footer: '<a href="#">Why do I have this issue?</a>'
+            text: "Estamos teniendo problemas técnicos, vuelva a intentar más adelante",
+            // footer: '<a href="#">Why do I have this issue?</a>'
 });
     }
 }
 cargarProductos(); 
+
+//! FILTRO DE PRODUCTOS
 
 const buscador = document.getElementById("buscador");
 
@@ -68,9 +40,7 @@ let total = 0
 let cantidad = 0
 
 
-//! DIV PRD
-
-//cargar prods visibles en html, que se agreguen al carrito y que se modifiquen subtotales
+//! CARDS DE PRODUCTOS
 
 const cargarDOM = (listaProductos = productos) => {
     const articulos = document.getElementById("articulos");
@@ -94,10 +64,6 @@ const cargarDOM = (listaProductos = productos) => {
             </div>`;
             
             const button = div.querySelector("button.btn");
-            // let button = document.createElement("button");
-            // button.className = "btn";
-            // button.innerHTML = "Agregar al carrito";
-            
 
             button.addEventListener("click", () => {
                 const selectTalle = document.getElementById(`talle-${producto.id}`);
@@ -106,47 +72,11 @@ const cargarDOM = (listaProductos = productos) => {
                 actualizarCarrito();
             });
 
-            // div.appendChild(button);
             articulos.appendChild(div);
         });
     };
 
-// const cargarDOM = () => {
-
-//     const articulos = document.getElementById("articulos")
-
-//     productos.forEach((producto) =>{
-//         let div = document.createElement("div");
-//         div.className = "producto";
-//         div.innerHTML = `
-//         <h3>${producto.nombre}</h3>
-//         <p>$${producto.precio}</p>
-//     <p>Talles disponibles: ${producto.talle.join(", ")}</p>`;
-        
-//         let button = document.createElement("button");
-//         button.className="btn";
-//         button.innerHTML="Agregar al carrito";
-
-//         button.addEventListener("click", () =>{
-//             agregarCarrito(producto);
-//             // alert(`${producto.nombre.toUpperCase()}" ha sido agregado al carrito"`);
-//             // actualizarCantidad();
-//             // cantidad+=1;
-//             actualizarCarrito ();
-//             total+= producto.precio*cantidad; 
-//             const subtotal = document.getElementById("subtot");
-//         // subtotal.innerHTML=`
-//         // <p>Productos totales: ${cantidad}</p>
-//         // <p>Subtotal: $${total}</p>`           
-//         });
-
-//         div.appendChild(button)
-//         articulos.appendChild(div);
-// })}
-
-//! DIV CRT
-
-//Que me vaya sumando productos y me tire un popup (hacer funcion del popup):
+//! SECCION CARRITO + SWEET ALERT 
 
 const agregarCarrito = (producto, talle) => {
     const itemAgregado = carrito.find (item => item.nombre === producto.nombre && item.talle == talle);
@@ -157,23 +87,9 @@ const agregarCarrito = (producto, talle) => {
     else
         carrito.push({...producto, cantidad:1, talle: talle});
     Swal.fire("Producto agregado al carrito!");
-
-    // mostrarPopup("El producto se ha agregado al carrito");
 }
 
-//popup
-
-// function mostrarPopup(mensaje) {
-//     const popup = document.getElementById("popup");
-//     const textoPop = document.getElementById("popup-text");
-
-//     textoPop.textContent = mensaje;
-//     popup.style.display = "flex";
-
-//     setTimeout(() => {
-//     popup.style.display = "none";
-//     }, 2000);
-// }
+//! FUNCIONES ANEXAS
 
 function calcularSubtotal() {
     let subtotal = 0;
@@ -190,10 +106,6 @@ function calcularCantidadTotal() {
     });
     return totalUnidades;
 }
-
-
-//Que se muestre la lista al costado 
-//TODO (me falta agregarlo al button event):
 
 const actualizarCarrito = () =>{
     const prodsCarrito = document.getElementById("carrito");
@@ -215,41 +127,15 @@ const actualizarCarrito = () =>{
         </div>
         <div class="preciocarrito">
         $${item.precio*item.cantidad}
-        </div>`; //! AGREGARLE EVENT A LOS BOTONES
+        </div>`; 
         
 prodsCarrito.appendChild(div);
+
+//! BOTONES + Y - EN CARRITO CON SWEET ALERT
 
 const botonMenos = document.querySelectorAll(".menos");
 const botonMas = document.querySelectorAll(".mas");
 
-// botonMenos.forEach((btn) => {
-// btn.addEventListener("click", (e) => {
-// let prodCarrito = carrito.find((prod) => prod.id == e.target.dataset.id);
-// if (prodCarrito.cantidad > 1) {
-// prodCarrito.cantidad--;
-// total -= prodCarrito.precio;
-// cantidad--;
-// } else {
-// carrito = carrito.filter((prod) => prod.nombre !== prodCarrito.nombre);
-// cantidad--;
-//     Swal.fire({
-//     title: "Producto eliminado",
-//     text: `Has eliminado ${item.nombre} del carrito`,
-//     icon: "warning",
-//     showCancelButton: true,
-//     confirmButtonColor: "#3085d6",
-//     cancelButtonColor: "#d33",
-//     confirmButtonText: "Entendido"
-//     }).then((result) => {
-//     if (result.isConfirmed) {
-//         Swal.fire({
-//         title: "Eliminado!",
-//         text: "Tu producto ha sido eliminado",
-//         icon: "success"
-//         });
-//     }
-// });
-// }
 botonMenos.forEach((btn) => {
 btn.addEventListener("click", async (e) => {
 let prodCarrito = carrito.find((prod) => prod.id == e.target.dataset.id);
@@ -293,109 +179,20 @@ total += prodCarrito.precio;
 cantidad++;
 actualizarCarrito();
 });
-})
-// botonMenos.forEach((btn) => {
-//     btn.addEventListener("click", () =>{
-//         if (item.cantidad > 1){
-//             item.cantidad--;
-//                 total -= item.precio;
-//                 cantidad--;
-//                 console.log("menosif");
-                
-//             } 
-//             else {
-//                 carrito = carrito.filter((prod) => prod.nombre !== item.nombre);
-//                 cantidad--;
-//                 console.log("menoselse")
-//             }
-//             actualizarCarrito();
-//         });
-//     });
-    
-//     botonMas.forEach((btn)=>{
-//         btn.addEventListener("click", () => {
-//             item.cantidad++;
-//             total += item.precio;
-//             cantidad++;
-//             actualizarCarrito();
-//             console.log("mas");
-//             console.log(cantidad);
-            
-            
-//         });
-//     });
+});
 });
 const subtotal = calcularSubtotal();
 document.getElementById("subtot").innerHTML = `
-    <p>Subtotal: $${subtotal}</p>
-`;
+    <p>Subtotal: $${subtotal}</p>`;
+
 const cantidadTotal = calcularCantidadTotal();
 
 document.getElementById("subtot").innerHTML = `
     <p>Cantidad de productos: ${cantidadTotal}</p>
-    <p>Subtotal: $${subtotal}</p>
-`;
-
+    <p>Subtotal: $${subtotal}</p>`;
 };
-        //--------------------------------------------------------------------
-        
-        // const botonMenos = document.getElementsByClassName("menos");
-        // const botonMas = document.getElementsByClassName("mas");
 
-        // botonMenos.addEventListener("click", () =>{
-        //     if (item.cantidad > 1){
-        //         item.cantidad--;
-        //         total -= item.precio;
-        //         cantidad--;
-        //     } 
-        //     else {
-        //         carrito = carrito.filter((prod) => prod.nombre !== item.nombre);
-        //         cantidad--;
-        //     }
-        //     actualizarCarrito();
-
-        // botonMas.addEventListener("click", () =>{
-        //         item.cantidad ++;
-        //         total += item.precio;
-        //         cantidad++; 
-        //         actualizarCarrito();        
-        //     })
-        
-//-----------------------------------------------------------------
-
-            //     prodsCarrito.addEventListener ("click", (e) => {
-            //         if(e.target.classList.contains("mas")) {
-            //             console.log("Agregar producto");
-            //             item.cantidad ++;
-            //             total += item.precio;
-            //             cantidad++;
-            //             actualizarCarrito();
-            //     }
-            //     else if (e.target.classList.contains("menos")) {
-            //         console.log("Restar producto");
-            //         if (item.cantidad > 1){
-            //             item.cantidad -= 1;
-            //             total -= item.precio;
-            //             cantidad-=1;
-            //         } 
-            //         else {
-            //             carrito = carrito.filter((prod) => prod.nombre !== item.nombre);
-            //             cantidad--;
-            //         }
-            //         actualizarCarrito();
-            //     }
-            // })
-
-//--------------------------------------------------------------------
+//! EJECUTAR
 
 cargarDOM();
 actualizarCarrito();
-
-
-//TODO Eliminar productos no deseados o cantidades
-//TODO Agregar buscador (filtros)
-//Cambiar el diseño por algo mas lindo
-//Agregar header y footer
-//Ver como ponerle foto a los productos (js o html?)
-//TODO Ver como seleccionar talle (listener con css -> backgrownd color? y que se muestre en carrito (como?))
-
