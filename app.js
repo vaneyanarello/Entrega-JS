9cf78ba5e233fd95c5d7d26b9716529c4043ff68
Vanesa Yanarello <vane.yanarello@gmail.com>
2025-12-10 01:53:41 +0100

    Agregado de codigo de dto

--- app.js
+++ app.js
@@ -181,16 +181,54 @@ actualizarCarrito();
 });
 });
 });
-const subtotal = calcularSubtotal();
-document.getElementById("subtot").innerHTML = `
-    <p>Subtotal: $${subtotal}</p>`;
+
+// let subtotal = calcularSubtotal();
+// document.getElementById("subtot").innerHTML = `
+// <p>Subtotal: $${subtotal}</p>`;
+
+let subtotal = codigoAplicado ? calcularSubtotal() * 0.9 : calcularSubtotal();
+// document.getElementById("subtot").innerHTML = `
+//         <p>Cantidad de productos: ${cantidadTotal}</p>
+//         <p>Subtotal: $${subtotal}${codigoAplicado ? " (Descuento del 10% aplicado)" : ""}</p>`;
+
+console.log(subtotal, "1");
+
 
 const cantidadTotal = calcularCantidadTotal();
 
 document.getElementById("subtot").innerHTML = `
     <p>Cantidad de productos: ${cantidadTotal}</p>
-    <p>Subtotal: $${subtotal}</p>`;
+    <p id="subtotalp">Subtotal: $${subtotal}${codigoAplicado ? ` (10% off aplicado)` : ""}</p>`;
+    console.log(subtotal, "2");
 };
+
+
+// const buscador = document.getElementById("buscador");
+
+// buscador.addEventListener("input", () => {
+//     const texto = buscador.value.toLowerCase();
+//     const productosFiltrados = productos.filter(producto =>
+//         producto.nombre.toLowerCase().includes(texto)
+//     );
+//     cargarDOM(productosFiltrados);
+
+const dto = document.getElementById("descuento");
+const btnDto = document.getElementById("btndto");
+let codigoAplicado = false;
+
+btnDto.addEventListener("click", () => {
+    const codigo = dto.value.toLowerCase();
+    
+    if (codigo === "dto10") {
+        const subtotal = calcularSubtotal();
+        const subtotalDto = subtotal * 0.9;
+        codigoAplicado = true;
+
+        document.getElementById("subtotalp").innerHTML= `Subtotal: $${subtotalDto} (10% off aplicado)`;
+    } else {
+        Swal.fire("Código inválido");
+    };
+});
 
 //! EJECUTAR
 
